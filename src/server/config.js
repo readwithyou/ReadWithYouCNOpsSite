36d7320fcc7462ff5ecc245038da04d2f410b2dc
Xue <yonglinx@9801a78d8657.ant.amazon.com>
2018-06-29 11:39:37 +0800

    force to log out if token expired

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secret': 'da3057da-82df-4fbf-8dc2-69b41b901570',
-    'emailAccount': 'lessons@readwithyou.com',
-    'emailKey': 'MXPingguo#18'
+    'secret': 'superscrete',
+    'emailAccount': 'superscrete',
+    'emailKey': 'superscrete'
 };
